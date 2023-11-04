ae26b868b6a0853427b7c7b13b7e3ca991bc8b43
Diana Lumbantoruan <febrinalt123@gmail.com>
2023-11-04 19:29:13 +0700

    fix issue overflow

--- lib/view/detail_page.dart
+++ lib/view/detail_page.dart
@@ -31,112 +31,107 @@ class DetailPage extends StatelessWidget {
           },
         ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            right: 30,
-            left: 20,
-            bottom: 20
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                      height: 300,
-                      child: Image.network(
-                        foodItem.images[0],
-                        fit: BoxFit.cover,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              right: 30,
+              left: 20,
+              bottom: 20
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.network(
+                  foodItem.images[0],
+                  fit: BoxFit.fitHeight,
+                  width: MediaQuery.of(context).size.width,
+                  height: 160,
+                ),
+                Text(
+                  "Price : ${foodItem.price}",
+                  style: const TextStyle(
+                    fontFamily: 'Avenir', 
+                    fontWeight: FontWeight.w800,
+                    fontSize: 25
+                  ),
+                ),
+                const SizedBox(height: 15),
+                Text(
+                  "Fun Facts of That Related to ${foodItem.name} :",
+                  style: const TextStyle(
+                    fontFamily: 'Avenir', 
+                    fontWeight: FontWeight.w800,
+                    fontSize: 15
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: foodItem.funFacts.map((funFact) {
+                    return Text(
+                      funFact,
+                      textAlign: TextAlign.justify,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.normal
                       ),
-                    ),
-                ],
-              ),
-              Text(
-                "Price : ${foodItem.price}",
-                style: const TextStyle(
-                  fontFamily: 'Avenir', 
-                  fontWeight: FontWeight.w800,
-                  fontSize: 25
+                    );
+                  }).toList(),
                 ),
-              ),
-              const SizedBox(height: 30),
-              Text(
-                "Fun Facts of That Related to ${foodItem.name} :",
-                style: const TextStyle(
-                  fontFamily: 'Avenir', 
-                  fontWeight: FontWeight.w800,
-                  fontSize: 15
+                const SizedBox(height: 30),
+                Text(
+                  "Nutrition Facts of ${foodItem.name} :",
+                  style: const TextStyle(
+                    fontFamily: 'Avenir', 
+                    fontWeight: FontWeight.w800,
+                    fontSize: 15
+                  ),
                 ),
-              ),
-              const SizedBox(height: 8),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: foodItem.funFacts.map((funFact) {
-                  return Text(
-                    funFact,
-                    textAlign: TextAlign.justify,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.normal
-                    ),
-                  );
-                }).toList(),
-              ),
-              const SizedBox(height: 30),
-              Text(
-                "Nutrition Facts of ${foodItem.name} :",
-                style: const TextStyle(
-                  fontFamily: 'Avenir', 
-                  fontWeight: FontWeight.w800,
-                  fontSize: 15
-                ),
-              ),
-              const SizedBox(height: 8),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: foodItem.nutritionFacts.map((funFact) {
-                  return Text(
-                    funFact,
-                    textAlign: TextAlign.justify,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.normal
-                    ),
-                  );
-                }).toList(),
-              ),
-              const SizedBox(height: 30),
-              Text(
-                "Images of ${foodItem.name} :",
-                style: const TextStyle(
-                  fontFamily: 'Avenir', 
-                  fontWeight: FontWeight.w800,
-                  fontSize: 15
-                ),
-              ),
-              const SizedBox(height: 8),
-              SizedBox(
-                height: 150,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: foodItem.images.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.network(foodItem.images[index]),
+                const SizedBox(height: 8),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: foodItem.nutritionFacts.map((funFact) {
+                    return Text(
+                      funFact,
+                      textAlign: TextAlign.justify,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.normal
                       ),
-                    );      
-                  }
-                )
-              ),
-            ]
+                    );
+                  }).toList(),
+                ),
+                const SizedBox(height: 30),
+                Text(
+                  "Images of ${foodItem.name} :",
+                  style: const TextStyle(
+                    fontFamily: 'Avenir', 
+                    fontWeight: FontWeight.w800,
+                    fontSize: 15
+                  ),
+                ),
+                const SizedBox(height: 8),
+                SizedBox(
+                  height: 150,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: foodItem.images.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(foodItem.images[index]),
+                        ),
+                      );      
+                    }
+                  )
+                ),
+              ]
+            ),
           ),
-        ),
+        )
       )
     );
   }
